6bb46435e396a9bd245bc8d1412795d2ca1752a0
Albertbassey <aniediongetim@gmail.com>
2024-01-21 12:49:50 -0800

    nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * monty_nop - does not do anything.
+ * @head: pointer to head.
+ * @count_lines: current line number.
+ * Return: void
+ */
+void monty_nop(stack_t **head, unsigned int count_lines)
+{
+	(void) head;
+	(void) count_lines;
+}
